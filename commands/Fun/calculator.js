566ba43c94cd755525404ca800e082fb8fa709ee
NarutoGaming <88095348+NarutoCodm@users.noreply.github.com>
2022-04-02 15:21:33 +0800

    Add files via upload

---
+++ commands/Fun/calculator.js
@@ -0,0 +1,19 @@
+const { Message, Client } = require("discord.js");
+const simplydjs = require('simply-djs')
+module.exports = {
+    name: "calculator",
+    aliases: ['cal'],
+    permissions : [""],
+    category: "Utility",
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        simplydjs.calculator(message, {
+            embedColor: '#ff0000',
+        })
+    },
+};
